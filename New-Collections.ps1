3f3e6d4cc965783da0a5f1d674858edb433d6616
Casper Schjøtt <cschjtt@gmail.com>
2018-04-10 08:31:46 +0200

    Initial commit

---
+++ New-Collections.ps1
@@ -0,0 +1,11 @@
+#Creates empty Device Collection
+New-CMDeviceCollection -LimitingCollectionName 'All Desktop and Server Clients' -Name 'Test'
+
+#Creates a custom User Client setting and sets the value
+New-CMClientSetting -Name 'Admin User UDA Settings' -Type User
+Set-CMClientSetting -Name 'Admin User UDA Settings' -AllowUserAffinity $true
+
+#Creates custom User Client setting and sets the value and deploy it
+New-CMClientSetting -Name 'Cloud Distribution Point Access' -Type User
+Set-CMClientSetting -Name 'Cloud Distribution Point Access' -AllowCloudDistributionPoint $true -CloudService
+Start-CMClientSettingDeployment -ClientSettingName 'Cloud Distribution Point Access' -CollectionName 'All Users'
