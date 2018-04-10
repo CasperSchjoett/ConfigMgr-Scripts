3bc105f6a670f8916a02f69c8363e41d55dd7a71
Casper Schjøtt <cschjtt@gmail.com>
2018-04-10 16:26:02 +0200

    Initial commit

--- New-Collections.ps1
+++ New-Collections.ps1
@@ -9,3 +9,11 @@ Set-CMClientSetting -Name 'Admin User UDA Settings' -AllowUserAffinity $true
 New-CMClientSetting -Name 'Cloud Distribution Point Access' -Type User
 Set-CMClientSetting -Name 'Cloud Distribution Point Access' -AllowCloudDistributionPoint $true -CloudService
 Start-CMClientSettingDeployment -ClientSettingName 'Cloud Distribution Point Access' -CollectionName 'All Users'
+
+#Distribute applications
+Start-CMContentDistribution -ApplicationName 'Paint.net 4.0.21' -DistributionPointName 'CM01.SCHJOETT.LAB'
+Start-CMContentDistribution -ApplicationName '7-Zip 18.01' -DistributionPointName 'CM01.SCHJOETT.LAB'
+
+#Deploy applications
+New-CMApplicationDeployment -Name 'Paint.net 4.0.21' -CollectionName 'All Windows 10 Clients'
+New-CMApplicationDeployment -Name '7-Zip 18.01' -CollectionName 'All Windows 10 Clients'
