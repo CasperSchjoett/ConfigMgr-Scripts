7a5406ec37348ebfd4f1e5bb74097c1536810b8b
Casper Schjøtt <cschjtt@gmail.com>
2018-04-12 10:41:23 +0200

    Initial commit

--- New-Collections.ps1
+++ New-Collections.ps1
@@ -17,3 +17,7 @@ Start-CMContentDistribution -ApplicationName '7-Zip 18.01' -DistributionPointNam
 #Deploy applications
 New-CMApplicationDeployment -Name 'Paint.net 4.0.21' -CollectionName 'All Windows 10 Clients'
 New-CMApplicationDeployment -Name '7-Zip 18.01' -CollectionName 'All Windows 10 Clients'
+
+#Deploy packages
+Start-CMPackageDeployment -PackageName 'Notepad++ 7.5.6' -StandardProgramName 'Install - Notepad++ 7.5.6'´
+-CollectionName 'All Windows 10 Clients' -DeployPurpose Available
